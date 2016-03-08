ae70e551e7c4216c1fab79e2d29a61ce17595e8e
David Sisson <dsisson@google.com>
2016-03-07 20:46:42 -0800

    test/org/antlr/v4/test/runtime/cpp/BasePython2Test.java

--- runtime-testsuite/test/org/antlr/v4/test/runtime/cpp/BasePythonTest.java
+++ runtime-testsuite/test/org/antlr/v4/test/runtime/cpp/BasePythonTest.java
@@ -27,7 +27,7 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.antlr.v4.test.runtime.python;
+package org.antlr.v4.test.runtime.cpp;
 
 import org.antlr.v4.Tool;
 import org.antlr.v4.automata.ATNFactory;
